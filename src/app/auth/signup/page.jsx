2c4b9e615b2df77a3a99533c31ca16be51c05ef1
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-22 16:33:28 +0600

    install hero ui and gr logo

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const signUPPage = () => {
+    return (
+        <div>
+            <h1>this is sign up page</h1>
+        </div>
+    );
+};
+
+export default signUPPage;
